d9a2c9c5f937e781df0fe553b98344f2fede0772
Timur Mishiev <timurmishiev@gmail.com>
2020-05-04 23:56:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,8 +16,6 @@ def to_pig_latin(phrase)
   @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
 end
 
- def to_pig_latin(sentence)
-    sentence.split.collect { |word| piglatinize(word) }.join(" ")
-  end
+ 
 
 end #class end
